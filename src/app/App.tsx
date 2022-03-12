eae76102f48382111092e5826e301f47b9e691cc
denya54 <denis-khvesenya@yandex.com>
2022-03-12 17:19:42 +0300

    add component Login, use react-router-dom

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -13,6 +13,8 @@ import Container from '@mui/material/Container';
 import LinearProgress from '@mui/material/LinearProgress';
 import { Menu } from '@mui/icons-material';
 import { ErrorSnackbar } from '../components/ErrorSnackbar/ErrorSnackbar'
+import {Login} from "../features/Login/Login";
+import {Navigate, Route, Routes} from "react-router-dom";
 
 type PropsType = {
     demo?: boolean
@@ -36,8 +38,16 @@ function App({demo = false}: PropsType) {
                 {status === 'loading' && <LinearProgress/>}
             </AppBar>
             <Container fixed>
-                <TodolistsList demo={demo}/>
+
+                <Routes>
+                    <Route path={'/'} element={<TodolistsList demo={demo}/>}/>
+                    <Route path={'login'} element={<Login/>}/>
+                    <Route path={'404'} element={<h1 style={{textAlign: 'center'}}>404 Page not found</h1>}/>
+                    <Route path={'*'} element={<Navigate to={'404'}/>}/>
+                </Routes>
+
             </Container>
+
         </div>
     )
 }
